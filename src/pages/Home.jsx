8070c75f0e63e1e680b0001cef57ab089c34fe7a
Ashiq <robiulislam.1711@gmail.com>
2024-01-09 15:36:00 +0600

    modify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='flex justify-center items-center h-screen text-red-500 text-[100px]'>This Is A Home Page</div>
+    <div className='flex justify-center items-center h-screen text-red-500 text-[100px]'>This Is A Home Page...</div>
   )
 }
 
